9afd76b135d5a57cdcd49a17353a8586bbdac19c
Nightrate12 <88366081+Nightrate12@users.noreply.github.com>
2023-10-23 11:21:38 +0800

    Add files via upload

---
+++ js/script_dan_kitchen.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+    $('#image1').click(function(e){
+        e.preventDefault();
+        document.getElementById('image1').onclick=
+        document.getElementById('item_name').value='Utensils 1';
+        document.getElementById('price').value='1000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image2').onclick=
+    document.getElementById('item_name').value='Utensils 2';
+    document.getElementById('price').value='1000';
+});
+$('#image3').click(function(e){
+    e.preventDefault();
+    document.getElementById('image3').onclick=
+    document.getElementById('item_name').value='Utensils 3';
+    document.getElementById('price').value='4000';
+});
+$('#image4').click(function(e){
+    e.preventDefault();
+    document.getElementById('image4').onclick=
+    document.getElementById('item_name').value='Utensils 4';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image5').onclick=
+    document.getElementById('item_name').value='Utensils 5';
+    document.getElementById('price').value='1000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image6').onclick=
+    document.getElementById('item_name').value='Utensils 6';
+    document.getElementById('price').value='1200';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image7').onclick=
+    document.getElementById('item_name').value='Utensils 7';
+    document.getElementById('price').value='1000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image8').onclick=
+    document.getElementById('item_name').value='Utensils 8';
+    document.getElementById('price').value='1000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image9').onclick=
+    document.getElementById('item_name').value='Utensils 9';
+    document.getElementById('price').value='900';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image10').onclick=
+    document.getElementById('item_name').value='Utensils 10';
+    document.getElementById('price').value='2000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image11').onclick=
+    document.getElementById('item_name').value='Utensils 11';
+    document.getElementById('price').value='1000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image12').onclick=
+    document.getElementById('item_name').value='Utensils 12';
+    document.getElementById('price').value='1500';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image13').onclick=
+    document.getElementById('item_name').value='Utensils 13';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image14').onclick=
+    document.getElementById('item_name').value='Utensils 14';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image15').onclick=
+    document.getElementById('item_name').value='Utensils 15';
+    document.getElementById('price').value='10000';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image16').onclick=
+    document.getElementById('item_name').value='Utensils 16';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image17').onclick=
+    document.getElementById('item_name').value='Utensils 17';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image18').onclick=
+    document.getElementById('item_name').value='Utensils 18';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image19').onclick=
+    document.getElementById('item_name').value='Utensils 19';
+    document.getElementById('price').value='800';
+});
+$('#image2').click(function(e){
+    e.preventDefault();
+    document.getElementById('image20').onclick=
+    document.getElementById('item_name').value='Utensils 20';
+    document.getElementById('price').value='800';
+});
+});
