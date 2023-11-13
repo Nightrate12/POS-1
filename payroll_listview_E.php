575c17c91a745e04715f95035c6619701857e7c4
Nightrate12 <88366081+Nightrate12@users.noreply.github.com>
2023-11-13 11:50:06 +0800

    Add files via upload

---
+++ payroll_listview_E.php
@@ -0,0 +1,102 @@
+<?php
+include 'process/db_connection.php';
+$conn = OpenCon();
+$sql = "SELECT * FROM `incometbl`";
+$result = $conn->query($sql);
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payroll List View</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script src="js/payroll_listview.js"></script>
+
+    <style>
+        table tr:hover {
+            cursor: pointer;
+
+        }
+
+        table thead {
+            background: maroon;
+        }
+
+        table thead tr th {
+            color: #fff;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h2 style="margin-top: 30px; margin-bottom:5px;">Employee's
+            Payroll List</h2>
+        <form id="form-users-list" action="" method="POST">
+            <div style="float: right;">
+                <span style="margin-top:5px;">Employee Number:</span>
+                <input type="text" style="margin-top:5px;" id="employee_no" name="employee_no">
+                <button id="search" type="submit" name="search_btn" class="btn btn-danger" style="padding:5px; margin-bottom:5px;">SEARCH</button>
+            </div>
+            <table class="table table-bordered table-hover" id="user_table">
+                <thead>
+                    <tr>
+                        <th>Employee Number</th>
+                        <th>Employee Name</th>
+                        <th>Basic Income</th>
+                        <th>Honorarium Income</th>
+                        <th>Other Income</th>
+                        <th>Gross Income</th>
+                        <th>Total Deduction</th>
+                        <th>Net Income</th>
+                        <th>Paydate</th>
+                    </tr>
+                </thead>
+                </tbody>
+                <?php
+                if ($result) {
+                    while ($item = $result->fetch_assoc()) {
+                        echo "
+                    <tr>
+                        <td class='py-6 ps-6'>$item[id]</td>
+                        <td class='py-6 ps-6'>$item[employee_no]</td>
+                        <td class='py-6 ps-6'>$item[income_date]</td>
+                        <td class='py-6 ps-6'>$item[basic_rate_hour]</td>
+                        <td class='py-6 ps-6'>$item[basic_num_hrs]</td>
+                        <td class='py-6 ps-6'>$item[basic_income]</td>
+                        <td class='py-6 ps-6'>$item[hono_rate_hour]</td>
+                        <td class='py-6 ps-6'>$item[hono_num_hrs]</td>
+                        <td class='py-6 ps-6'>$item[hono_income]</td>
+                        <td class='py-6 ps-6'>$item[other_rate_hour]</td>
+                        <td class='py-6 ps-6'>$item[other_num_hrs]</td>
+                        <td class='py-6 ps-6'>$item[other_income]</td>
+                        <td class='py-6 ps-6'>$item[gross_income]</td>
+                        <td class='py-6 ps-6'>$item[net_income]</td>
+                    </tr>
+                    ";
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+    </div>
+    </form>
+</body>
+</html>
