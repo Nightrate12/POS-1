445c462a6093a821b8ec89200eedc651b292c4db
Nightrate12 <88366081+Nightrate12@users.noreply.github.com>
2023-11-13 11:22:25 +0800

    Add files via upload

--- js/payroll_listview.js
+++ js/payroll_listview.js
@@ -1,10 +1,10 @@
 $document.ready(function(){
     $('table tr').click(function(){
         var id= $(this).attr('row_id');
-        window.open("http://localhost/lpu_web_application/employee_registration_update.php?id=" +id);
+        window.open("http://localhost/lpu_web_application/payroll_lab4.php?id=" +id);
     });
     $('#search').click(function(e){
         var employee_no = $('#employee_no').val();
-        window.open("http//localhost/lpu_web_application/employee_listview.php?search=" + employee_no);
+        window.open("http//localhost/lpu_web_application/payroll_listview_search.php?search=" + employee_no);
     });
 });
